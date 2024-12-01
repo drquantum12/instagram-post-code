b95acb00cb4c45d16cacfb1e1267fa77fb35c3ae
arjun singh tomar <arjunsinghtomar03511@gmail.com>
2024-12-01 22:17:48 +0530

    moving completed anova project

--- anova/utility.js
+++ anova/utility.js
@@ -1,9 +1,10 @@
 const n_containers = 3;
-const n_particles = 100;
+const n_particles = 10;
 const particleColors = ["red", "blue", "green"];
 // const particleColors = ["#F28C8C", "#87CEEB", "#A8D5BA"];
 const container_arr = Array();
-const velocity = 2;
+const velocity = 4;
+var global_mean = 0;
 
 class particleContainer{
     constructor(n_particles,x, y, height, width, particle_color, ctx){
@@ -15,7 +16,7 @@ class particleContainer{
         this.width=width;
         this.particle_color=particle_color;
         this.ctx = ctx;
-        this.v_sum = 0;
+        this.v_mean = 0;
 
         this.draw = () =>{
             for(let i=0; i<n_particles; i++){
@@ -119,7 +120,7 @@ const setup = () => {
 
 
     for(let i=0; i<n_containers; i++){
-        const container = new particleContainer(n_particles, container_xpos, 50, container_height, container_width, particleColors[i], ctx);
+        const container = new particleContainer(n_particles, container_xpos, 150, container_height, container_width, particleColors[i], ctx);
         container_arr.push(container);
         container_xpos += container_width + 20;
         container.draw();
@@ -143,19 +144,100 @@ const update_container_particles = () => {
             }
 
             particle.draw();
-            container_v_sum+= Math.sqrt(particle.vx*particle.vx + particle.vy*particle.vy);
+            particle.v = Math.sqrt(particle.vx*particle.vx + particle.vy*particle.vy);
+            container_v_sum+= particle.v;
         });
+        container.v_mean = container_v_sum/container.particleArray.length;
 
-        
+        global_mean = globalMean(container_arr);
+
+        let msw = cal_msw(container_arr);
+        let msb = cal_msb(container_arr);
+
         ctx.beginPath();
         ctx.rect(container.x_pos, container.y_pos, container.width, container.height);
         ctx.strokeStyle = "green";
         ctx.stroke();
         ctx.fillStyle="Black";
+
+        ctx.font = "50px Arial";
+        ctx.fillText("ANOVA Test on particle velocities in three containers.", 50, 50);
+
+        ctx.font = "10px Arial";
+        ctx.fillText("MSB : ", 50, 100);
+        ctx.fillText(msb, 100, 100);
+
+        ctx.fillText("MSW : ", 250, 100);
+        ctx.fillText(msw, 300, 100);
+
+        ctx.fillText("F - statistic : ", 450, 100);
+        ctx.fillText((msb/msw), 530, 100);
+
         ctx.fillText("Average Container Velocity", container.x_pos-50+(container.width/2), container.y_pos-30);
-        ctx.fillText(container_v_sum, container.x_pos-50+(container.width/2), container.y_pos-10);
+        ctx.fillText(container.v_mean, container.x_pos-50+(container.width/2), container.y_pos-10);
     });
 
     
     requestAnimationFrame(update_container_particles);
 };
+
+
+// ----------- anova ---------------
+
+// ssw = E(X_i - Mean_i)^2
+const cal_ssw = (container_arr) =>{
+    let res = 0;
+    container_arr.forEach(container=>{
+        let temp_res = 0;
+        container.particleArray.forEach(particle=>{
+            temp_res += (particle.v - container.v_mean)*(particle.v - container.v_mean);
+        });
+        res += temp_res;
+    });
+    return res
+}
+
+const globalMean = (container_arr) => {
+    let temp_sum = 0;
+    container_arr.forEach(container=>{
+        temp_sum += container.v_mean;
+    });
+
+    return temp_sum/n_containers;
+}
+
+// mean square within (net variance with groups)
+// msw = ssw/(N-num_groups)
+const cal_msw = (container_arr) => {
+    let res = cal_ssw(container_arr);
+    // sum of square with in group over degree of freedom (dim -1)
+    return res/((n_particles-1)*n_containers);
+}
+
+const cal_ssb = (container_arr) => {
+    let res = 0;
+    container_arr.forEach(container=>{
+        res += n_particles*(container.v_mean - global_mean)*(container.v_mean - global_mean);
+    });
+    return res;
+}
+
+// mean square between (net variance between the groups)
+// msb
+const cal_msb = (container_arr) => {
+    let res = cal_ssb(container_arr);
+    return res/(n_containers-1);
+}
+
+// as per the law of variances
+// total variance should be equivalent to SS_total = ssb + ssw
+const cal_sst = (container_arr) => {
+    let temp_sum=0;
+    container_arr.forEach(container=>{
+        container.particleArray.forEach(particle=>{
+            temp_sum += (particle.v-global_mean)*(particle.v-global_mean);
+        });
+    });
+    return temp_sum;
+}
+
